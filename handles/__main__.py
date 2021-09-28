f3cae3e17830f7f7088d187a8592fffa9c14fd7a
Elia Giacobazzi <elia.giacobazzi@euler.it>
2021-09-28 13:00:44 +0200

    Create first working prototype

---
+++ handles/__main__.py
@@ -0,0 +1,87 @@
+import dataclasses
+import pathlib
+
+import solid
+import solid.utils
+
+from .config import load_toml
+
+
+@dataclasses.dataclass
+class Config:
+    pass
+
+
+def button_hole(total_width: float, diameter: float, **kwargs) -> solid.OpenSCADObject:
+    center_distance = (total_width - diameter) / 2
+    return solid.hull()(
+        solid.utils.left(center_distance)(solid.circle(d=diameter, **kwargs)),
+        solid.utils.right(center_distance)(solid.circle(d=diameter, **kwargs)),
+    )
+
+
+def button_hole_3d(
+    total_width: float, diameter: float, depth: float, **kwargs
+) -> solid.OpenSCADObject:
+    return solid.linear_extrude(depth)(button_hole(total_width, diameter, **kwargs))
+
+
+CONFIG_PATH = pathlib.Path("./config")
+OUTPUT_PATH = pathlib.Path("./output")
+
+config = load_toml(CONFIG_PATH / "handles.toml", Config)
+
+
+# inner Hole
+inner_width = 100
+inner_height = 35
+
+
+# External and internal mask pressing over cardboard
+mask_border = 12
+mask_depth = 2
+mask_width = inner_width + 2 * mask_border
+mask_height = inner_height + 2 * mask_border
+
+mask = button_hole(mask_width, mask_height)
+mask = solid.linear_extrude(mask_depth)(mask)
+
+
+# External part main body
+external_border = 3.5
+external_depth = 17
+external_width = inner_width + 2 * external_border
+external_height = inner_height + 2 * external_border
+
+external = button_hole(external_width, external_height)
+external = solid.linear_extrude(external_depth)(external)
+
+external_hole = button_hole(inner_width, inner_height)
+external_hole = solid.hole()(solid.linear_extrude(external_depth)(external_hole))
+
+
+# Internal part main body
+internal_border = 1.5
+internal_depth = 10
+internal_width = external_width + 2 * internal_border
+internal_height = external_height + 2 * internal_border
+
+internal = button_hole(internal_width, internal_height)
+internal = solid.linear_extrude(internal_depth)(internal)
+
+internal_hole = button_hole(external_width, external_height)
+internal_hole = solid.hole()(solid.linear_extrude(internal_depth)(internal_hole))
+
+
+external_part = mask + external - external_hole
+solid.scad_render_to_file(external_part, OUTPUT_PATH / "external.scad")
+
+
+internal_part = mask + internal - internal_hole
+solid.scad_render_to_file(internal_part, OUTPUT_PATH / "internal.scad")
+
+
+full = solid.union()
+full.add(solid.utils.forward(mask_height / 2 + 5)(external_part))
+full.add(solid.utils.back(mask_height / 2 + 5)(internal_part))
+solid.scad_render_to_file(full, OUTPUT_PATH / "full.scad")
