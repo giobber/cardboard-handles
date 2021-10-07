20d0b2b05ef41a88b150810fac47f73efe1d5c7d
Elia Giacobazzi <elia.giacobazzi@euler.it>
2021-10-07 16:38:35 +0200

    Create config class for handles

--- models/handles/config.py
+++ models/handles/config.py
@@ -3,4 +3,28 @@ import dataclasses
 
 @dataclasses.dataclass
 class Config:
-    pass
+    hole_width: float = 90
+    hole_height: float = 30
+
+    mask_border: float = 10
+    body_border: float = 3
+    lock_border: float = 2
+
+    mask_depth: float = 2
+    lock_depth: float = 8
+    hang_depth: float = 2
+
+    tolerance: float = 1
+    cardboard_depth: float = 2
+
+    compose_full: bool = False
+
+    @property
+    def body_depth(self):
+        return (
+            self.mask_depth
+            + self.cardboard_depth
+            + self.mask_depth
+            + self.lock_depth
+            + self.hang_depth
+        )
