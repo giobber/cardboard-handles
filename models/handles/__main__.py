8f9d03892f001e6be70fedf04c2af519fbfacaa0
Elia Giacobazzi <elia.giacobazzi@euler.it>
2021-10-07 16:38:54 +0200

    Improve handles script with config and utilities

--- models/handles/__main__.py
+++ models/handles/__main__.py
@@ -1,77 +1,49 @@
-import pathlib
-
-import solid
 import solid.utils
 from loguru import logger
 
 from models.system.config import load_toml
 from models.system.constants import CONFIG_PATH, OUTPUT_PATH
-from models.system.models import button_hole, button_hole_3d, pill
+from models.system.models import button_hole
+from models.system.utils import render, set_color
 
 from .config import Config
 
-config = load_toml(CONFIG_PATH / "handles.toml", Config)
+c = load_toml(CONFIG_PATH / "handles.toml", Config)
+
+# External part
+mask = button_hole(c.hole_width, c.hole_height, c.mask_border, c.mask_depth)
+body = button_hole(c.hole_width, c.hole_height, c.body_border, c.body_depth)
+
+external = mask + body
+external = set_color(external, "ForestGreen")
 
 
-# inner Hole
-# TODO: calculate external depth based on cardboard depth
-inner_width = 100
-inner_height = 35
-cardboard_depth = 6
+# Internal part
+offset = c.body_border + c.tolerance
+
+width = c.hole_width + 2 * offset
+height = c.hole_height + 2 * offset
+border = c.mask_border - offset
+
+mask = button_hole(width, height, border, c.mask_depth)
+lock = button_hole(width, height, c.lock_border, c.lock_depth + c.mask_depth)
+
+internal = mask + lock
+internal = set_color(internal, "Lime")
 
 
-# External and internal mask pressing over cardboard
-mask_border = 12
-mask_depth = 2
-mask_width = inner_width + 2 * mask_border
-mask_height = inner_height + 2 * mask_border
-
-
-# External part main body
-# TODO: missing border clip to handle internal part
-external_border = 3.5
-external_depth = 17
-external_width = inner_width + 2 * external_border
-external_height = inner_height + 2 * external_border
-
-mask = button_hole_3d(
-    external_width, external_height, mask_border - external_border, mask_depth
-)
-external = button_hole_3d(inner_width, inner_height, external_border, external_depth)
-external_part = mask + external
-external_part = solid.color("ForestGreen")(external_part)
-
-
-# Internal part main body
-# TODO: missing tolerance to let parts slice together
-internal_border = 1.5
-internal_depth = 10
-internal_width = external_width + 2 * internal_border
-internal_height = external_height + 2 * internal_border
-
-mask = button_hole_3d(
-    internal_width,
-    internal_height,
-    mask_border - internal_border - external_border,
-    mask_depth,
-)
-internal = button_hole_3d(
-    external_width, external_height, internal_border, internal_depth
-)
-internal_part = mask + internal
-internal_part = solid.color("Lime")(internal_part)
-
-
-# Generate a composed solid
+# Complete design
 full = solid.union()
-# full.add(solid.system.forward(mask_height / 2 + 5)(external_part))
-# full.add(solid.system.back(mask_height / 2 + 5)(internal_part))
-full.add(external_part)
-full.add(solid.utils.up(mask_depth + cardboard_depth)(internal_part))
+if c.compose_full:
+    offset = c.hole_height / 2 + c.mask_border + 5
+    full.add(solid.utils.forward(offset)(external))
+    full.add(solid.utils.back(offset)(internal))
+else:
+    full.add(external)
+    full.add(solid.utils.up(c.mask_depth + c.cardboard_depth)(internal))
 
 
-# Render
-logger.info("Rendering...")
-solid.scad_render_to_file(external_part, OUTPUT_PATH / "external.scad")
-solid.scad_render_to_file(internal_part, OUTPUT_PATH / "internal.scad")
-solid.scad_render_to_file(full, OUTPUT_PATH / "full.scad")
+# Rendering
+render(external, OUTPUT_PATH / "external.scad")
+render(internal, OUTPUT_PATH / "internal.scad")
+render(full, OUTPUT_PATH / "full.scad")
