00e5b833629c1211242182bf185866940c3a0085
Elia Giacobazzi <elia.giacobazzi@euler.it>
2021-10-11 18:18:20 +0200

    Add colors

--- models/basstrap/__main__.py
+++ models/basstrap/__main__.py
@@ -16,20 +16,25 @@ depth: float = support_width
 width: float = support_width + hang_height
 
 base = solid.square((width, thickness))
+base = extrude(base, depth=depth + thickness)
+base = set_color(base, "green")
 
 hang = solid.square((hang_height * math.sqrt(2), thickness))
 hang = rotate_xy(hang, 90 + 45)
 hang = translate(hang, x=width, y=thickness)
+hang = extrude(hang, depth=depth + thickness)
+hang = set_color(hang, "blue")
 
-draw = base + hang
-
-result = extrude(draw, depth=depth)
+leg = solid.square((thickness, hang_height))
+leg = translate(leg, x=support_width)
+leg = extrude(leg, thickness)
+leg = set_color(leg, "purple")
 
 screw_hole = solid.cylinder(d=screw_hole_diameter, h=thickness, segments=64)
 screw_hole = rotate(screw_hole, axis_x=-90)
 screw_hole = translate(screw_hole, x=support_width / 2, z=support_width / 2)
-screw_hole = set_color(screw_hole, "blue")
+screw_hole = set_color(screw_hole, "red")
 
-result -= screw_hole
+result = base + hang + leg - screw_hole
 
 render(result, OUTPUT_PATH / "basstrap.scad")
